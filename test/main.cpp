209620bf154ce8a144056475afad048af70253a8
bottindorian <ETUDIANT_2A@PCSN3.(none)>
2023-12-14 08:13:44 +0100

    ajoute d'un texte personalisé

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout << "Hello dorian!" << endl;
+    string a;
+    cout << "Hello !" << endl;
+    cin>>a;
+    cout<<a<<endl;
     return 0;
 }
